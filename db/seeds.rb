8de9cf58436ab26d63865f5cbc7ec7dee3ea20b4
wenzheng-Zhu <zwz_322@126.com>
2023-04-05 18:01:52 +0800

    ad db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Teacher.destroy_all
+Student.destroy_all
+Follow.destroy_all
+
+
+Teacher.create!([{
+                   name: "Jack",
+                   phone_no: '11111111111',
+                 },
+                 {
+                   name: "Smith",
+                   phone_no: '22222222222',
+                 },
+                ])
+
+
+Student.create!([{
+                   name: "Jack",
+                   phone_no: '11111111111',
+                   studying_id: 'asd'
+                 },
+                 {
+                   name: "Smith",
+                   phone_no: '22222222222',
+                   studying_id: 'ert'
+                 },
+                ])
+
+t = Teacher.first
+s1 = Student.first
+s2 = Student.last
+
+
+Student.create!([{
+                   teacher_id: t.id,
+                   student_id: s1.id,
+                 },
+                 {
+                   teacher_id: t.id,
+                   student_id: s2.id,
+                 },
+                ])
+
+
